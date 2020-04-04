bce45ac14da6f1490f513acc47f92e23c1d2c430
camnangcuoi <camnangcuoi2018@gmail.com>
2020-04-04 21:43:13 +0700

    lession 2

--- src/Buoi02_BrowserCommands.java
+++ src/Buoi02_BrowserCommands.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BrowserCommands {
+public class Buoi02_BrowserCommands {
 
     public static void main (String [] args)
     {
