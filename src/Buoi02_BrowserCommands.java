8e18f69e5443071b33c33a163ec7aa6f5bd4689d
camnangcuoi <camnangcuoi2018@gmail.com>
2020-04-05 11:31:47 +0700

    update lession 2

--- src/Buoi02_BrowserCommands.java
+++ src/Buoi02_BrowserCommands.java
@@ -3,15 +3,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Buoi02_BrowserCommands {
 
-    public static void main (String [] args)
-    {
+    public static void main (String [] args) throws InterruptedException {
         String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath);
 
         System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         //Trình duyệt truy cập vào google.com
+        //Phương thức GET sẽ ko back lại đc trang trc đó
         driver.get("https://google.com");
 
         //In giá trị Title của trình duyêt ra màn hình
@@ -26,22 +27,29 @@ public class Buoi02_BrowserCommands {
         String pageSource = driver.getPageSource();
         System.out.println(pageSource);
 
-        //Thực hiện truy cập FB
-        driver.navigate().to("https://facebook.com");
-
         //Refresh trang web
         driver.navigate().refresh();
+
+        Thread.sleep(1000);
+
+        //Thực hiện truy cập FB
+        //Phương thức TO cho phép back về trang trc đó
+        driver.navigate().to("https://facebook.com");
 
         //Back về trang trước đó
         driver.navigate().back();
 
         //Forward về trang trước đó
-        driver.navigate().forward();
+        //driver.navigate().forward();
+        //System.out.println("Forward!");
 
         //Phóng to trang web
         driver.manage().window().maximize();
 
-        //Đóng trình duyệt browser
+        //Đóng tab/window hiện tại của trình duyệt đang mở
          driver.close();
+
+         //Đóng tất cả các tab của trình duyệt
+        //driver.quit();
     }
 }
